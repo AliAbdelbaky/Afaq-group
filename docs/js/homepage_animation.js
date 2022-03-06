daf010aafa3c1dc8a63f9789c96ad88f377982a5
Ali Abdelbaky <ali.abdelbaky2000@gmail.com>
2022-03-07 00:40:50 +0200

    gitpages

--- docs/js/homepage_animation.js
+++ docs/js/homepage_animation.js
@@ -1,6 +1,14 @@
 // variables
 let header = document.querySelector(".mainHeader");
 let candidtesSec = document.querySelector(".candidates");
+let briefSec = document.querySelector(".brief");
+let electoral_programSec = document.querySelector(".electoral_program");
+let electoral_targetSec = document.querySelector(".electoral_target");
+let afaqGroupSec = document.querySelector(".afaqGroup");
+let afaqTweetSec = document.querySelector(".afaqTweet");
+let eventDetailSec = document.querySelector(".eventDetails");
+let tech_supportSec = document.querySelector(".tech_support");
+let footer = document.querySelector("footer");
 gsap.timeline({
         scrollTrigger: {
             trigger: header,
@@ -18,9 +26,86 @@ gsap.timeline({
             start: "top bottom",
             end: "+=100 bottom",
             scrub: 2,
-            markers: true,
         }
     })
     .fromTo(candidtesSec.querySelector(".main_sec_title"), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" })
     .fromTo(candidtesSec.querySelector(".owl-carousel"), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" }, "-=0.5");
-console.log(candidtesSec)
+gsap.timeline({
+        scrollTrigger: {
+            trigger: briefSec,
+            start: "top bottom",
+            end: "+=200 bottom",
+            scrub: 2,
+        }
+    })
+    .fromTo(briefSec.querySelector(".main_sec_title"), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" })
+    .fromTo(briefSec.querySelector("p"), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" }, "-=0.5");
+gsap.timeline({
+        scrollTrigger: {
+            trigger: electoral_programSec,
+            start: "top bottom",
+            end: "+=100 bottom",
+            scrub: 2,
+        }
+    })
+    .fromTo(electoral_programSec.querySelector(".main_sec_title"), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" })
+    .fromTo(electoral_programSec.querySelector(".owl-carousel"), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" }, "-=0.5");
+gsap.timeline({
+        scrollTrigger: {
+            trigger: electoral_targetSec,
+            start: "top bottom",
+            end: "+=100 bottom",
+            scrub: 2,
+        }
+    })
+    .fromTo(electoral_targetSec.querySelector(".main_sec_title"), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" })
+    .fromTo(electoral_targetSec.querySelector(".owl-carousel"), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" }, "-=0.5");
+gsap.timeline({
+        scrollTrigger: {
+            trigger: afaqGroupSec,
+            start: "top bottom",
+            end: "+=100 bottom",
+            scrub: 2,
+        }
+    })
+    .fromTo(afaqGroupSec.querySelector(".main_sec_title"), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" })
+    .fromTo(afaqGroupSec.querySelector(".owl-carousel"), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" }, "-=0.5");
+gsap.timeline({
+        scrollTrigger: {
+            trigger: afaqTweetSec,
+            start: "top bottom",
+            end: "+=100 bottom",
+            scrub: 2,
+        }
+    })
+    .fromTo(afaqTweetSec.querySelector("h2"), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" })
+    .fromTo(afaqTweetSec.querySelector(".qouat_container"), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" }, "-=0.5");
+gsap.timeline({
+        scrollTrigger: {
+            trigger: eventDetailSec,
+            start: '+=100 bottom',
+            end: '+=100 top',
+            scrub: 2,
+        }
+    })
+    .fromTo(eventDetailSec.querySelector(".main_sec_title"), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" })
+    .fromTo(eventDetailSec.querySelector(".carousel"), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" }, "-=0.5");
+gsap.timeline({
+        scrollTrigger: {
+            trigger: tech_supportSec,
+            start: '+=100 bottom',
+            end: '+=100 top',
+            scrub: 2,
+        }
+    })
+    .fromTo(tech_supportSec.querySelector(".form_container"), { x: "100%", opacity: "0" }, { x: "0%", opacity: "1" })
+    .fromTo(tech_supportSec.querySelector("img"), { x: "-100%", opacity: "0" }, { x: "0%", opacity: "1" }, "-=0.5");
+gsap.timeline({
+        scrollTrigger: {
+            trigger: footer,
+            start: '-=150 bottom',
+            end: '+=100 top',
+            scrub: 1
+        }
+    })
+    .fromTo(footer, { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" })
