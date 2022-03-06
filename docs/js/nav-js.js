4efa5cf21da2153897d6987734d56ecd61b46880
Ali Abdelbaky <ali.abdelbaky2000@gmail.com>
2022-03-06 21:34:39 +0200

    gitpages

--- docs/js/nav-js.js
+++ docs/js/nav-js.js
@@ -4,30 +4,18 @@ window.addEventListener('scroll', () => {
     this.scrollY >= 150 ? navbar.classList.add("navScroll") : navbar.classList.remove("navScroll");
     this.scrollY >= 150 ? sidebar.classList.add("navScroll") : sidebar.classList.remove("navScroll");
 })
-$(function() {
-    App.init();
-});
-var App = {
-    init: function() {
-        this.side.nav()
-    },
-    side: {
-        nav: function() {
-            this.toggle(), this.navigation()
-        },
-        toggle: function() {
-            $(".navbar-toggler").on("touchstart click", function(e) {
-                e.preventDefault(), $(".sidebar").toggleClass("active"), $(".nav").removeClass("active"), $(".sidebar .sidebar-overlay").removeClass("fadeOut animated").addClass("fadeIn animated")
-            }), $(".sidebar .sidebar-overlay").on("touchstart click", function(e) {
-                e.preventDefault(), $(".navbar-toggler").click(), $(this).removeClass("fadeIn").addClass("fadeOut")
-            })
-        },
-        navigation: function() {
-            $(".nav-left a").on("touchstart click", function(e) {
-                e.preventDefault();
-                var t = $(this).attr("href").replace("#", "");
-                $(".sidebar").toggleClass("active"), $(".html").removeClass("visible"), "home" == t || "" == t || null == t ? $(".html.welcome").addClass("visible") : $(".html." + t).addClass("visible"), App.title($(this).text())
-            })
-        }
-    },
-};
+let sidebar = document.querySelector(".sidebar");
+let buttonToggler = document.querySelector('nav button');
+let sidebar_overlay = sidebar.querySelector('.sidebar-overlay');
+let navLink = document.querySelectorAll('.sidebar .sidebar-content a')
+sidebar_overlay.addEventListener("click", () => {
+    sidebar.classList.remove('active')
+})
+buttonToggler.addEventListener("click", () => {
+    sidebar.classList.toggle('active')
+})
+navLink.forEach(item => {
+    item.addEventListener('click', () => {
+        sidebar.classList.remove('active')
+    })
+})
