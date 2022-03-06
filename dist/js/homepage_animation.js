f967f41a616fab04db7aa166d7594ada63540ee6
Ali Abdelbaky <ali.abdelbaky2000@gmail.com>
2022-03-07 00:01:24 +0200

    homepage_animation

--- dist/js/homepage_animation.js
+++ dist/js/homepage_animation.js
@@ -1,15 +1,26 @@
 // variables
 let header = document.querySelector(".mainHeader");
-console.log(header)
+let candidtesSec = document.querySelector(".candidates");
 gsap.timeline({
         scrollTrigger: {
             trigger: header,
-            start: "+=400 center",
+            start: "+=600 center",
             end: "bottom center",
             scrub: 1,
-            markers: true
         }
     })
-    .fromTo("header .title", { x: "0%" }, { x: "100%", opacity: "0" })
-    .fromTo("header .btn_container", { x: "0%" }, { x: "100%", opacity: "0" })
-    .fromTo("header .img_container", { x: "0%" }, { x: "-100%", opacity: "0" });
+    .to(header.querySelector(".title"), { y: "100%", opacity: "0" })
+    .to(header.querySelector(".btn_container"), { y: "100%", opacity: "0" }, "-=0.5")
+    .to(header.querySelector(".img_container"), { y: "-100%", opacity: "0" }, "-=0.5");
+gsap.timeline({
+        scrollTrigger: {
+            trigger: candidtesSec,
+            start: "top bottom",
+            end: "+=100 bottom",
+            scrub: 2,
+            markers: true,
+        }
+    })
+    .fromTo(candidtesSec.querySelector(".main_sec_title"), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" })
+    .fromTo(candidtesSec.querySelector(".owl-carousel"), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" }, "-=0.5");
+console.log(candidtesSec)
