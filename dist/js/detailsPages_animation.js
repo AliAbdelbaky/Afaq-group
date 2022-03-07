7c38ed5c40d0eae77b6aa955f4887b71d5e539cb
Ali Abdelbaky <ali.abdelbaky2000@gmail.com>
2022-03-07 05:41:33 +0200

    detals_page_animation

---
+++ dist/js/detailsPages_animation.js
@@ -0,0 +1,33 @@
+let header = document.querySelector('.secHeader')
+let skill_reviewSec = document.querySelector('.skill_review');
+gsap.timeline({
+        scrollTrigger: {
+            trigger: header,
+            start: "+=600 center",
+            end: "bottom center",
+            scrub: 2
+        }
+    })
+    .to(header, { opacity: "0" });
+gsap.timeline({
+        scrollTrigger: {
+            trigger: skill_reviewSec.querySelector('.membership'),
+            start: 'top bottom',
+            end: '+=100 bottom',
+            scrub: 2
+        }
+    })
+    .fromTo(skill_reviewSec.querySelector('.membership .main-title'), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" })
+    .fromTo(skill_reviewSec.querySelector('.membership .skill-review-items'), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" })
+    .fromTo(skill_reviewSec.querySelector('.vision'), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" })
+    .fromTo(skill_reviewSec.querySelector('.msg'), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" })
+gsap.timeline({
+        scrollTrigger: {
+            trigger: skill_reviewSec.querySelector('.goals'),
+            start: 'top bottom',
+            end: '+=300 bottom',
+            scrub: 2
+        }
+    })
+    .fromTo(skill_reviewSec.querySelector('.title'), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" })
+    .fromTo(skill_reviewSec.querySelector('.goal-list'), { y: "100%", opacity: "0" }, { y: "0%", opacity: "1" })
